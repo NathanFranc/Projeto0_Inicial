6ffd9fe60e740fe64c98a7593f31712cf2c5c88c
limavinicius195 <limavinicius195@gmail.com>
2025-03-02 16:28:37 -0300

    Meu primeiro commit

---
+++ PPJJ.py
@@ -0,0 +1,22 @@
+print("ola e um bom dia")
+
+print("jornada de dados")
+
+# Variaveis 
+idade = 24
+nome = "jubileu"
+
+# Print 
+print("idade")
+
+
+# Soma
+nova_idade = idade + 1
+print("nova_idade")
+
+
+# lista 
+lista = [1,2,3,4,5,6,7]
+
+#metodos
+lista_invertida = lista.reverse()
